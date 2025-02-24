823741d896a2c4c561e3c8bae1da751622f9cd79
Prasad <ppkhose6@gmail.com>
2025-02-24 11:51:22 +0530

    Implemented sign-in and sign-up form handling with input fields.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,32 +1,55 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [isNew, setNew] = useState(false);
+
   return (
     <div>
       <img
-        className="brightness-35"
+        className="brightness-50"
         src="https://assets.nflxext.com/ffe/siteui/vlv3/f268d374-734d-474f-ad13-af5ba87ef9fc/web/IN-en-20250210-TRIFECTA-perspective_92338d5d-6ccd-4b1a-8536-eb2b0240a55e_small.jpg"
         alt="bg-img"
       ></img>
       <h1 className="absolute top-5 left-40 z-10 text-red-600 font-medium text-5xl">
         NetflixGPT
       </h1>
-      <div className="absolute top-30 left-120 z-10 w-120 bg-[rgba(6,2,2,0.65)] p-12 rounded-[10px]">
-        <h1 className="text-white text-3xl font-bold">Sign In</h1>
+      <div className="absolute top-30 left-120 z-10 w-120 bg-[rgba(6,2,2,0.7)] p-12 rounded-[10px]">
+        <h1 className="text-white text-3xl font-bold">
+          {isNew ? "Sign Up" : "Sign In"}
+        </h1>
         <form className="justify-center mt-6 p-5 flex flex-col gap-4">
+          {isNew && (
+            <input
+              className="w-full bg-[rgba(123,123,177,0.4)] p-3 rounded-[5px] text-white"
+              type="text"
+              placeholder="Name"
+              required={true}
+            />
+          )}
           <input
-            className="w-full bg-gray-200 p-3 rounded-[5px] opacity-70"
+            className="w-full bg-[rgba(123,123,177,0.4)] p-3 rounded-[5px] text-white"
             type="email"
-            placeholder="Email or Mobile Number"
+            placeholder="Email"
             required={true}
           />
           <input
-            className="w-full bg-gray-200 p-3 rounded-[5px] opacity-70"
+            className="w-full bg-[rgba(123,123,177,0.4)]  p-3 rounded-[5px] text-white"
             type="password"
             placeholder="Password"
             required={true}
           />
           <button className="bg-red-600 text-white font-medium w-full text-center mt-6 py-3 rounded-[5px] hover:bg-red-700 cursor-pointer">
-            Sign In
+            {isNew ? "Sign Up" : "Sign In"}
           </button>
+          <p className="text-gray-400 mt-4">
+            {isNew ? "Already have an account ?" : "New to NetflixGPT ?"}
+            <span
+              className="text-white hover:underline cursor-pointer"
+              onClick={() => setNew(!isNew)}
+            >
+              {isNew ? " Login." : " Sign Up now."}
+            </span>
+          </p>
         </form>
       </div>
     </div>
